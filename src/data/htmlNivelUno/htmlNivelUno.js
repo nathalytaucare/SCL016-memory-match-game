f1be8a99ff79c29246fb4442ef265ff7ee0798f2
mailys50 <maiysbrizuela@gmail.com>
2021-03-15 12:07:48 -0300

    data e imagenes

---
+++ src/data/htmlNivelUno/htmlNivelUno.js
@@ -0,0 +1,23 @@
+export default {
+    name: 'htmlNivelUno',
+    items: [
+      { id: 'htmlNivel1,1,1', image: 'cartas/HTML nivel 1/agua.png', class:"carta1" },
+      { id: 'htmlNivel1,1,2', image: 'cartas/HTML nivel 1/agua1.png', class:"carta1" },
+      { id: 'htmlNivel1,2,1', image: 'cartas/HTML nivel 1/amarillo.png', class:"carta2"  },
+      { id: 'htmlNivel1,2,2', image: 'cartas/HTML nivel 1/amarillo1.png', class:"carta2"  },
+      { id: 'htmlNivel1,3,1', image: 'cartas/HTML nivel 1/azul.png', class:"carta3"  },
+      { id: 'htmlNivel1,3,2', image: 'cartas/HTML nivel 1/azul1.png', class:"carta3"  },
+      { id: 'htmlNivel1,4,1', image: 'cartas/HTML nivel 1/crema.png', class:"carta4" },
+      { id: 'htmlNivel1,4,2', image: 'cartas/HTML nivel 1/crema1.png', class:"carta4" },
+      { id: 'htmlNivel1,5,1', image: 'cartas/HTML nivel 1/lila.png' , class:"carta5" },
+      { id: 'htmlNivel1,5,2', image: 'cartas/HTML nivel 1/lila1.png' , class:"carta5" },
+      { id: 'htmlNivel1,6,1', image: 'cartas/HTML nivel 1/marron.png', class:"carta6"  },
+      { id: 'htmlNivel1,6,2', image: 'cartas/HTML nivel 1/marron1.png', class:"carta6"  },
+      { id: 'htmlNivel1,7,1', image: 'cartas/HTML nivel 1/rojo.png', class:"carta7"  },
+      { id: 'htmlNivel1,7,2', image: 'cartas/HTML nivel 1/rojo1.png', class:"carta7"  },
+      { id: 'htmlNivel1,8,1', image: 'cartas/HTML nivel 1/verde.png', class:"carta8" },
+      { id: 'htmlNivel1,8,2', image: 'cartas/HTML nivel 1/verde1.png', class:"carta8" },
+      
+    ],
+    
+  };
---
+++ src/data/jsNivelDos/jsNivelDos.js
@@ -0,0 +1,23 @@
+export default {
+    
+    name: 'jsNivelDos',
+    items: [                        
+      { id: 'jsNivel2,1,1', image: 'cartas/JS nivel 2/agua.png', class:"carta1" },
+      { id: 'jsNivel2,1,2', image: 'cartas/JS nivel 2/agua1.png', class:"carta1" },
+      { id: 'jsNivel2,1,2', image: 'cartas/JS nivel 2/amarillo.png', class:"carta2"  },
+      { id: 'jsNivel2,2,2', image: 'cartas/JS nivel 2/amarillo1.png', class:"carta2"  },
+      { id: 'jsNivel2,3,1', image: 'cartas/JS nivel 2/azul.png', class:"carta3"  },
+      { id: 'jsNivel2,3,2', image: 'cartas/JS nivel 2/azul1.png', class:"carta3"  },
+      { id: 'jsNivel2,4,1', image: 'cartas/JS nivel 2/crema.png', class:"carta4" },
+      { id: 'jsNivel2,4,2', image: 'cartas/JS nivel 2/\crema1.png', class:"carta4" },
+      { id: 'jsNivel2,5,1', image: 'cartas/JS nivel 2/lila.png' , class:"carta5" },
+      { id: 'jsNivel2,5,2', image: 'cartas/JS nivel 2/lila1.png' , class:"carta5" },
+      { id: 'jsNivel2,6,1', image: 'cartas/JS nivel 2/marron.png', class:"carta6"  },
+      { id: 'jsNivel2,6,2', image: 'cartas/JS nivel 2/marron1.png', class:"carta6"  },
+      { id: 'jsNivel2,7,1', image: 'cartas/JS nivel 2/rojo.png', class:"carta7"  },
+      { id: 'jsNivel2,7,2', image: 'cartas/JS nivel 2/rojo1.png', class:"carta7"  },
+      { id: 'jsNivel2,8,1', image: 'cartas/JS nivel 2/verde.png', class:"carta8" },
+      { id: 'jsNivel2,8,2', image: 'cartas/JS nivel 2/verde1.png', class:"carta8" },
+      
+    ],
+  };
