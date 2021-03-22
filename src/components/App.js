bd20a01ed7b2da4c7e2101ae5232ec65cd76ba23
Nathaly Taucare <nathaly.tau@gmail.com>
2021-03-22 09:44:34 -0300

    Html y Css mejorados, funcionamiento botones js

--- src/components/App.js
+++ src/components/App.js
@@ -6,8 +6,6 @@
 // console.log(pokemon);
 import htmlNivelUno from '../data/htmlNivelUno/htmlNivelUno.js';
 console.log(htmlNivelUno);
-import htmlNivelDos from '../data/htmlNivelDos/htmlNivelDos.js';
-console.log(htmlNivelDos);
 //
 // O alternativamente podríamos cargar el JSON de forma asíncrona usando
 // `fetch` en el momento que consideremos necesario.
--- src/main.js
+++ src/main.js
@@ -1,3 +1,22 @@
 import App from './components/App.js';
+const buttonHtml =document.getElementById("buttonHtml"); 
+const startButton = document.getElementById("startButton");
+const buttonHtmlEnd =document.getElementById("buttonHtmlEnd");
+
+const secondPage = () => {                                                    
+    document.querySelector('.homePage').style.display = "none";
+    document.querySelector('.endPage').style.display="none"
+    document.querySelector('.htmlCategory').style.display = "block";
+}
+
+buttonHtml.addEventListener("click", secondPage); 
+buttonHtmlEnd.addEventListener("click",secondPage);
+
+const homePage = () => {                                                    
+    document.querySelector('.homePage').style.display = "block";
+    document.querySelector('.htmlCategory').style.display = "none";
+    document.querySelector('.endPage').style.display="none"
+}
+startButton.addEventListener("click", homePage);  
 
 document.getElementById('root').appendChild(App());
