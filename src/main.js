b07f22baa22ea88b06a83dd5c3b05c88e5148774
mailys50 <maiysbrizuela@gmail.com>
2021-03-23 12:36:28 -0300

    funcioncartas

--- src/main.js
+++ src/main.js
@@ -1,24 +1,65 @@
 
 import App from './components/App.js';
+// import AppCss from './components/AppCss.js';
+// // import AppJs from './components/AppJs.js';
+
 const buttonHtml =document.getElementById("buttonHtml"); 
-const startButton = document.getElementById("startButton");
+const startButtonHtml = document.getElementById("startButtonHtml");
 const buttonHtmlEnd =document.getElementById("buttonHtmlEnd");
+const buttonCss =document.getElementById("buttonCss"); 
+const startButtonCss = document.getElementById("startButtonCss");
+const buttonCssEnd =document.getElementById("buttonCssEnd");
+const buttonJs =document.getElementById("buttonJs"); 
+const startButtonJs = document.getElementById("startButtonJs");
+const buttonJsEnd =document.getElementById("buttonJsEnd");
 
 const secondPage = () => {                                                    
     document.querySelector('.homePage').style.display = "none";
     document.querySelector('.endPage').style.display="none"
     document.querySelector('.htmlCategory').style.display = "block";
+    document.querySelector('.cssCategory').style.display = "none";
+    document.querySelector('.jsCategory').style.display = "none";
 }
 
 buttonHtml.addEventListener("click", secondPage); 
 buttonHtmlEnd.addEventListener("click",secondPage);
 
+
+const thirdPage = () => {                                                    
+  document.querySelector('.homePage').style.display = "none";
+  document.querySelector('.endPage').style.display="none"
+  document.querySelector('.htmlCategory').style.display = "none";
+  document.querySelector('.cssCategory').style.display = "block";
+  document.querySelector('.jsCategory').style.display = "none";
+}
+
+buttonCss.addEventListener("click", thirdPage); 
+buttonCssEnd.addEventListener("click",thirdPage);
+
+const quarterPage = () => {                                                    
+  document.querySelector('.homePage').style.display = "none";
+  document.querySelector('.endPage').style.display="none"
+  document.querySelector('.htmlCategory').style.display = "none";
+  document.querySelector('.cssCategory').style.display = "none";
+  document.querySelector('.jsCategory').style.display = "block";
+}
+
+buttonJs.addEventListener("click", quarterPage); 
+buttonJsEnd.addEventListener("click",quarterPage);
+
 const homePage = () => {                                                    
     document.querySelector('.homePage').style.display = "block";
     document.querySelector('.htmlCategory').style.display = "none";
+    document.querySelector('.cssCategory').style.display = "none";
+    document.querySelector('.jsCategory').style.display = "none";
     document.querySelector('.endPage').style.display="none"
 }
-startButton.addEventListener("click", homePage);  
+startButtonHtml.addEventListener("click", homePage); 
+startButtonCss.addEventListener("click", homePage); 
+startButtonJs.addEventListener("click", homePage); 
+
+
+
 
 
 //cronometro
@@ -26,16 +67,13 @@ startButton.addEventListener("click", homePage);
             // cargaCartas();
             temporizador();
         };
-        // function desordenarArrays(arrayX){
-        //     let arrayReacomodado = arrayX.sort(function(){return Math.random() -0.5});
-        //     return arrayReacomodado;
-        // }
+       
         function temporizador(){
           let tiempo=0;
             temporizador = setInterval(() => {
                
                 tiempo++
-                document.getElementById('temporizador').innerHTML = tiempo;
+                document.getElementById('timer').innerHTML = tiempo;
                 if (tiempo==6) {
                   myStopFunction();
                 }
@@ -50,3 +88,5 @@ startButton.addEventListener("click", homePage);
 
 
 document.getElementById("root").appendChild(App());
+// document.getElementById("root1").appendChild(AppCss());
+// document.getElementById("root2").appendChild(AppJs());
